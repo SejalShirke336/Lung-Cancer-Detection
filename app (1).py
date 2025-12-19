22c084139976f57f093d75e5f6d4b211d0d6b86e
Sejal Shirke <shirkesejal618@gmail.com>
2025-12-19 19:57:10 +0530

    app (1).py

--- app (1).py
+++ app (1).py
@@ -114,21 +114,26 @@ if st.button("🔍 Predict Lung Cancer"):
 
     data = pd.DataFrame(0, index=[0], columns=model_columns)
 
-    data["AGE"] = AGE
-    data["GENDER_M"] = 1 if GENDER == "Male" else 0
-    data["SMOKING"] = 1 if SMOKING == "Yes" else 0
-    data["YELLOW_FINGERS"] = 1 if YELLOW_FINGERS == "Yes" else 0
-    data["ANXIETY"] = 1 if ANXIETY == "Yes" else 0
-    data["PEER_PRESSURE"] = 1 if PEER_PRESSURE == "Yes" else 0
-    data["CHRONIC_DISEASE"] = 1 if CHRONIC_DISEASE == "Yes" else 0
-    data["FATIGUE"] = 1 if FATIGUE == "Yes" else 0
-    data["ALLERGY"] = 1 if ALLERGY == "Yes" else 0
-    data["WHEEZING"] = 1 if WHEEZING == "Yes" else 0
-    data["ALCOHOL_CONSUMING"] = 1 if ALCOHOL_CONSUMING == "Yes" else 0
-    data["COUGHING"] = 1 if COUGHING == "Yes" else 0
-    data["SHORTNESS_OF_BREATH"] = 1 if SHORTNESS_OF_BREATH == "Yes" else 0
-    data["SWALLOWING_DIFFICULTY"] = 1 if SWALLOWING_DIFFICULTY == "Yes" else 0
-    data["CHEST_PAIN"] = 1 if CHEST_PAIN == "Yes" else 0
+    def set_feature(col_name, value):
+    if col_name in model_columns:
+        data[col_name] = value
+
+set_feature("AGE", AGE)
+set_feature("GENDER_MALE", True if GENDER == "Male" else False)
+
+set_feature("SMOKING", 1 if SMOKING == "Yes" else 0)
+set_feature("YELLOW_FINGERS", 1 if YELLOW_FINGERS == "Yes" else 0)
+set_feature("ANXIETY", 1 if ANXIETY == "Yes" else 0)
+set_feature("PEER_PRESSURE", 1 if PEER_PRESSURE == "Yes" else 0)
+set_feature("CHRONIC DISEASE", 1 if CHRONIC_DISEASE == "Yes" else 0)
+set_feature("FATIGUE", 1 if FATIGUE == "Yes" else 0)
+set_feature("ALLERGY", 1 if ALLERGY == "Yes" else 0)
+set_feature("WHEEZING", 1 if WHEEZING == "Yes" else 0)
+set_feature("ALCOHOL CONSUMING", 1 if ALCOHOL_CONSUMING == "Yes" else 0)
+set_feature("COUGHING", 1 if COUGHING == "Yes" else 0)
+set_feature("SHORTNESS OF BREATH", 1 if SHORTNESS_OF_BREATH == "Yes" else 0)
+set_feature("SWALLOWING DIFFICULTY", 1 if SWALLOWING_DIFFICULTY == "Yes" else 0)
+set_feature("CHEST PAIN", 1 if CHEST_PAIN == "Yes" else 0)
 
     prediction = model.predict(data)
 
